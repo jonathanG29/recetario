31f29fbb87177f9d5257f159dcc4c4f9c40b6580
jonathan gallego <jonathan.gallego7@gmail.com>
2024-05-02 07:25:42 -0500

    feature: Registro de usuarios compleatdo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import authRoutes from './routes/auth.routes.js'
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json()); // esto permite que el back entienda las peticiones en formato json y se exporta desde express
+
+app.use("/api", authRoutes)
 
 export default app;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { login, register } from '../controllers/auth.controller.js'
 
 const router = Router()
 
 //creamos las rutas
-router.post('/register')
-router.post('login')
+router.post('/register', register);
+
+router.post('/login', login);
+
+
+export default router
