26d84a4ac16888c4f047a0d4ff0585ceb1518158
jonathan gallego <jonathan.gallego7@gmail.com>
2024-05-03 10:40:23 -0500

    Feature: se agrega ruta perfil y se protegen rutas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json()); // esto permite que el back entienda las peticiones en formato json y se exporta desde express
+app.use(cookieParser());
 
 app.use("/api", authRoutes)
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { login, register } from '../controllers/auth.controller.js'
+import { login, register, logout, profile } from '../controllers/auth.controller.js'
+import { authRequired } from '../middlewares/validateToken.js'
 
 const router = Router()
 
@@ -8,5 +9,8 @@ router.post('/register', register);
 
 router.post('/login', login);
 
+router.post('/logout', logout);
+
+router.get('/profile', authRequired, profile);
 
 export default router
