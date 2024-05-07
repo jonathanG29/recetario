bd9f68d4ef87603d680b4afa42dd03c8173fd758
jonathan gallego <jonathan.gallego7@gmail.com>
2024-05-07 07:09:35 -0500

    se agrega campo al model de usuarios para recetas

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -14,7 +14,12 @@ const userSchema =  new mongoose.Schema({
     password:{
         type: String,
         required: true
-    }
+    },
+    recipiefav:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Recipies",
+        default: null,
+    },
 },{
     timestamps: true
 })
